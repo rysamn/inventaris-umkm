ec18ec3690d11b90033ec201f0537c224293d426
rysamn <fahrysaefulanwar25@gmail.com>
2025-11-12 11:14:50 +0700

    first commit

---
+++ src/main/java/com/umkm/inventaris/InventarisApplication.java
@@ -0,0 +1,13 @@
+package com.umkm.inventaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarisApplication.class, args);
+	}
+
+}
