bf1f08a64669dcc9da632c65479c07e6bf39b198
rysamn <fahrysaefulanwar25@gmail.com>
2025-11-22 19:26:42 +0700

    clean code

--- src/main/java/com/umkm/inventaris/dao/PembelianDao.java
+++ src/main/java/com/umkm/inventaris/dao/PembelianDao.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/umkm/inventaris/dao/PenjualanDao.java
+++ src/main/java/com/umkm/inventaris/dao/PenjualanDao.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
